9bdc7fd726999f687372680e8aad6d1d1ecc9773
Sofia Johansson <sj224ds@student.lnu.se>
2025-09-05 11:55:33 +0200

    Add welcome message

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/**
+ * The main script file of the application.
+ *
+ * @author Sofia Nord <sj224ds@student.lnu.se>
+ * @version 1.0.0
+ */
+
+const userInput = document.getElementById('nameInput')
+const submitBtn = document.getElementById('submitBtn')
+const welcomeDiv = document.getElementById('welcomeMessageContainer')
+
+// sätt upp eventlyssnare på submit-knappen
+submitBtn.addEventListener('click', () => {
+  submitBtn.disabled = true
+  const inputName = userInput.value
+
+  const greeting = document.createElement('h2')
+  const greetingContent = document.createTextNode(`Welcome ${inputName}!`)
+  greeting.appendChild(greetingContent)
+
+  const greetingDiv = document.createElement('div')
+  greetingDiv.appendChild(greeting)
+  document.body.appendChild(greetingDiv)
+})
+
+// göm översta diven
+welcomeDiv.classList.add('hidden')
